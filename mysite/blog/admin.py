500803e3d4153b449f84a190711d1924363557da
jfahadi <jfahadi@gmail.com>
2015-10-24 16:20:01 +0300

    My django Girls App,First commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Post
+
+# Register your models here to make it visible on the page
+
+admin.site.register(Post)
---
+++ mysite/blog/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns=[
+    url(r'^$',views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+]
+
+
+
+
+
+    #url(r'', include('blog.urls')),
+    #url(r'^admin/', include(admin.site.urls)),]
